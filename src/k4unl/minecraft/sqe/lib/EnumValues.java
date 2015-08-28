8ae2fffccb364081490eb09e623f91a41711e155
Koen Beckers <koen@k-4u.nl>
2015-08-28 23:49:37 +0200

    Started on enumValues

---
+++ src/k4unl/minecraft/sqe/lib/EnumValues.java
@@ -0,0 +1,14 @@
+package k4unl.minecraft.sqe.lib;
+
+/**
+ * @author Koen Beckers (K-4U)
+ */
+public enum EnumValues {
+    TIME(0x0),;
+
+    char theNumber;
+
+    EnumValues(int i) {
+        theNumber = (char)i;
+    }
+}
